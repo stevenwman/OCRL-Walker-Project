f8b72ec04f29779afc1c3330e972769c3189034c
Steven Man <sman2@andrew.cmu.edu>
2024-04-18 17:31:25 -0400

    adding dynamics to traj opt

--- hybrid_traj_opt_5link/DynamicSystems/constraints.jl
+++ hybrid_traj_opt_5link/DynamicSystems/constraints.jl
@@ -1,34 +1,36 @@
 include("biped5link.jl") # Defines hybrid system functions
 
-function stance1_dynamics(params::NamedTuple, x::Vector, u::Vector)
+function discrete_stance1_dynamics(params::NamedTuple, x::Vector, u::Vector, dt::Real)::Vector
     # dynamics when foot 1 is in contact with the ground 
-    
-    mb,mf = model.mb, model.mf
-    g = model.g
 
-    M = Diagonal([mb mb mf mf mf mf])
+    # x, y, t1, t2, t3, t4, t5 = q
+    # xd, yd, t1d, t2d, t3d, t4d, t5d = q̇
     
-    rb  = x[1:2]   # position of the body
-    rf1 = x[3:4]   # position of foot 1
-    rf2 = x[5:6]   # position of foot 2
-    v   = x[7:12]  # velocities
+    q, q̇ = x[1:7], x[8:14]
+
+    kkt_newton_step(q, q̇, u, params, fpos, left_foot_constraint, dt)
+    
+    # rb  = x[1:2]   # position of the body
+    # rf1 = x[3:4]   # position of foot 1
+    # rf2 = x[5:6]   # position of foot 2
+    # v   = x[7:12]  # velocities
     
     
-    ℓ1x = (rb[1]-rf1[1])/norm(rb-rf1)
-    ℓ1y = (rb[2]-rf1[2])/norm(rb-rf1)
-    ℓ2x = (rb[1]-rf2[1])/norm(rb-rf2)
-    ℓ2y = (rb[2]-rf2[2])/norm(rb-rf2)
+    # ℓ1x = (rb[1]-rf1[1])/norm(rb-rf1)
+    # ℓ1y = (rb[2]-rf1[2])/norm(rb-rf1)
+    # ℓ2x = (rb[1]-rf2[1])/norm(rb-rf2)
+    # ℓ2y = (rb[2]-rf2[2])/norm(rb-rf2)
     
-    B = [ℓ1x  ℓ2x  ℓ1y-ℓ2y;
-         ℓ1y  ℓ2y  ℓ2x-ℓ1x;
-          0    0     0;
-          0    0     0;
-          0  -ℓ2x  ℓ2y;
-          0  -ℓ2y -ℓ2x]
+    # B = [ℓ1x  ℓ2x  ℓ1y-ℓ2y;
+    #      ℓ1y  ℓ2y  ℓ2x-ℓ1x;
+    #       0    0     0;
+    #       0    0     0;
+    #       0  -ℓ2x  ℓ2y;
+    #       0  -ℓ2y -ℓ2x]
     
-    v̇ = [0; -g; 0; 0; 0; -g] + M\(B*u)
+    # v̇ = [0; -g; 0; 0; 0; -g] + M\(B*u)
     
-    ẋ = [v; v̇]
+    # ẋ = [v; v̇]
     
     return ẋ
 end
