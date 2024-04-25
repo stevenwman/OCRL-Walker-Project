4f44889afdfa648c69ebf60198a1fc55149a81bb
Steven Man <sman2@andrew.cmu.edu>
2024-04-24 21:28:42 -0400

    added new files and access

--- read_results.jl
+++ read_results.jl
@@ -11,7 +11,10 @@ data = JLD2.load("results.jld2")
 # println(data["X"])
 
 Xm = data["Xm"]
+Um = data["Um"]
+t_vec = data["params"].t_vec
 
 display(plot(Xm[1,:],Xm[2,:], label = "body"))
-display(plot(t_vec[1:end-1], Um',xlabel = "time (s)", ylabel = "U",
-                label = ["u1" "u2" "u3" "u4"], title = "Controls"))
+display(plot(t_vec[1:end-1], Um',
+             xlabel = "time (s)", ylabel = "U",
+             label = ["u1" "u2" "u3" "u4"], title = "Controls"))
